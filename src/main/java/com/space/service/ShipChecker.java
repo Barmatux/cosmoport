8ff93205a480445c4ceeef59094430e169a4d3eb
Barmatux <kupi1kupi@gmail.com>
2019-09-23 15:54:33 +0300

    Время

--- src/main/java/com/space/service/ShipChecker.java
+++ src/main/java/com/space/service/ShipChecker.java
@@ -2,13 +2,35 @@ package com.space.service;
 
 import com.space.exceptions.InCorrectFieldException;
 import com.space.model.Ship;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ShipChecker {
 
    private static double rating = 0.0;
+   private static SimpleDateFormat simpleDateFormat =new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+    private static Date date1;
+    private static Date date2 ;
+    private static Date date3;
 
-    public static void checkShip(Ship ship) throws InCorrectFieldException {
+    static {
+        try {
+
+            date1 = simpleDateFormat.parse("01/01/2800 00:00:00");
+            date2 = simpleDateFormat.parse("01/01/3020 00:00:00");
+            date3 = simpleDateFormat.parse("01/01/3019 00:00:00");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ShipChecker() throws ParseException {
+    }
+
+    public static void checkShip(Ship ship) throws InCorrectFieldException,  {
+
 
         if(ship.getId()<=0  || ship.getId()==(long)ship.getId()){
             throw new InCorrectFieldException();
@@ -24,13 +46,13 @@ public class ShipChecker {
             throw new InCorrectFieldException();
         }
 
-        if(ship.getProdDate().before(new Date("01.01.2800")) && ship.getProdDate().before(new Date("01.01.3020"))) {
+        if(ship.getProdDate().before(date1) && ship.getProdDate().after(date2)) {
             throw new InCorrectFieldException();
         }
-        if(ship.getSpeed()==0.0  || (ship.getSpeed()<0.01 && ship.getSpeed()>0.99 )){
+        if(ship.getSpeed()==null  || (ship.getSpeed()<0.01 && ship.getSpeed()>0.99 )){
             throw new InCorrectFieldException();
         }
-        if(ship.getCrewSize()==0  || (ship.getCrewSize()<1 && ship.getSpeed()>9999) ){
+        if(ship.getCrewSize()==null  || (ship.getCrewSize()<1 && ship.getSpeed()>9999) ){
             throw new InCorrectFieldException();
         }
 
@@ -39,7 +61,7 @@ public class ShipChecker {
     }
     public static void shipRating(Ship ship){
         rating = (80*ship.getSpeed()*(ship.getUsed()?0.5:1))/
-                (new Date("31.12.3019").getYear()-ship.getProdDate().getYear())+1;
+                ((date3).getYear()-ship.getProdDate().getYear())+1;
         rating=Math.round(rating*100.0)/100.0;
         ship.setRating(rating);
     }
